50befd1c4f2967e2b374c3b26ddd5b4d62d59c0b
kandd871 <114494325+kandd871@users.noreply.github.com>
2023-11-16 07:22:43 -0500

    updates

--- page2/main.js
+++ page2/main.js
@@ -1,34 +1,265 @@
 const data = [
-    { "user": "virtigex", "info": "Walk my dog early in the morning. We pretend we are navigating an apocalyptic landscape in the search for treats.", "color": "#EBFF00" },
-    { "user": "wertyuio234", "info": "Stargazing sets me straight.", "color": "#FA00FF" },
-    { "user": "[deleted]", "info": "stay up awfully late", "color": "#D9D9D9" },
-    { "user": "[deleted]", "info": "Read fantasy fiction.", "color": "#D9D9D9" },
-    { "user": "e-rekt-ion", "info": "Hmm isn’t that why we’re all here on Reddit?", "color": "red" },    
-    { "user": "GT_334", "info": "Shower....time flows slow in there for some reason", "color": "#56835A" },
-    { "user": "damnoice", "info": "daydream", "color": "#FFB774" },
-    { "user": "[deleted]", "info": "Write fiction. Read fiction. Gaming.", "color": "#D9D9D9" },
-    { "user": "northsideindian", "info": "Not today narc", "color": "#F2D642" },
-    { "user": "[deleted]", "info": "Drugs", "color": "#FF9DE3" },
-    { "user": "misstaken66", "info": "Listen to music", "color": "#007AFF" },
-    { "user": "Unhappy_Mongoose_778", "info": "Garden. Saved me from depression. No meds ever worked.", "color": "#5F76CB" },    
-    { "user": "Relative_Economics22", "info": "Sleep. Too depressed and broke to even try anything else", "color": "#36FF16" },
-    { "user": "TubzMcgee", "info": "Think about yo momma", "color": "#DCE4FF" },
-    { "user": "TheVginyTcikler44", "info": "Eat.", "color": "#B10000" },    
-    { "user": "MinnestoanPerson", "info": "Come here, and read.<br><br>Edit: just got back from school and saw this.", "color": "#29BF2F" },
-    { "user": "katzgar", "info": "youtube", "color": "#3B4C42" },
-    { "user": "DeionizableNormality", "info": "Imagine how things might get better in my life if I tweaked a few things, practice gratitude, meditate, read fiction, talk to my parents, learn about interesting things on Reddit or the web in general, audiobook while taking an evening walk. Sometimes I like to act as though nobody around me can see me and I slip into a child like curiosity.<br><br>The other day I was walking around my apartment complex in some kaki shorts and a plaid button down/my Fanny pack sashed around my shoulder. I went into the office area to try and find this back terrace I saw while walking around the building. I found myself in the gym where they had one of those Walmart brand peloton bikes. I decided… why not give it a go! Even though everyone was looking at me in the gym like I wasn’t supposed to be there, I went for a stationary two-mile bike ride while listening to my book. Happily got off after my ride, cleaned the machine, and went on my way to continue my adventure.<br><br>It seems to me that the quality of mind you’re carrying about with you will help a ton. It’s much better to slip back into that childlike curiosity than dwell on your adult life where circumstances are only moderately malleable. Slipping into that curiosity is what really makes all of these stories about people’s disconnection so salient. Whether it’s reading, daydreaming, gaming, etc. everyone is portraying a level of childlike curiosity about their lives and minds have to offer. It’s not so serious; enjoy this life and the people around you will enjoy theirs more too.", "color": "#FF8D3B" },
-    { "user": "Fbg_dello", "info": "Smoke weed", "color": "#89F1FF" },
-    { "user": "tarani5", "info": "SciFi or Fantasy movies", "color": "#FF00B8" },
-    { "user": "Casperios", "info": "Either escape into my own mind, or go into vr.<br><br>For me, they are kinda similar", "color": "#00C2FF" },
-    { "user": "palmni01", "info": "I find a cozy, somewhat isolated section of a trail or creek and smoke a cigar while listening to podcasts. Can't say it's the healthiest habit, but it gets me outdoors and makes me less anxious.", "color": "#4CA45A" },
-    { "user": "DragonKorny", "info": "Listen to an audiobook in a bed with a thick warm blanket", "color": "#9E22FF" },
-    { "user": "Ancient-Pause-99", "info": "watch tv", "color": "#745963" },
-    { "user": "[deleted]", "info": "it's hard work trying to find reality in this culture.<br><br>Everything is a distraction from the real world.", "color": "#D9D9D9" },
-    { "user": "TybroV", "info": "Nothing. There's no escape.", "color": "#FF9DE3" },
-    { "user": "dontjustexists", "info": "Video games", "color": "#1700A4" },
-    { "user": "D3SYNK", "info": "Late at night, I take my bike and ride to my old elementary school that's now a public park. I sit down on the same bench I've always sat at, and I start recording.<br><br> I've found that talking my problems out, or talking about anything, just helps me to escape. Some of those audio logs I've listened to countless times. I catch myself thinking \"wow, I actually said that.\" <br><br> Maybe someday I'll post them online, or preserve them as a memento to the times I\'ve lived through.<br><br> I can tell you right now, if I never started making those logs I wouldn't be half the person I am now.", "color": "#D9D9D9" },
-    { "user": "[deleted]", "info": "Dissociate", "color": "#D9D9D9" },
-    { "user": "purpleowlie", "info": "Exercise and if I have more time go for a longer hike and enjoy nature. I don't care how old I am I will always enjoy walking around forest, fallen leaves up to your knees, kicking them around, listening to rustling. And walking on freshly fallen snow.", "color": "#605CFF" },
+    {
+        "user": "virtigex",
+        "info": "Walk my dog early in the morning. We pretend we are navigating an apocalyptic landscape in the search for treats.",
+        "color": "#EBFF00"
+    },
+    {
+        "user": "wertyuio234",
+        "info": "Stargazing sets me straight.",
+        "color": "#FA00FF"
+    },
+    {
+        "user": "[deleted]",
+        "info": "stay up awfully late",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Read fantasy fiction.",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "GT_334",
+        "info": "Shower....time flows slow in there for some reason",
+        "color": "#56835A"
+    },
+    {
+        "user": "damnoice",
+        "info": "daydream",
+        "color": "#FFB774"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Write fiction. Read fiction. Gaming.",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "CoastalFred",
+        "info": "Skate",
+        "color": "#A763C7"
+    },
+    {
+        "user": "northsideindian",
+        "info": "Not today narc ",
+        "color": "#F2D642"
+    },
+    {
+        "user": "e-rekt-ion",
+        "info": "Hmm isn’t that why we’re all here on Reddit?",
+        "color": "#FF0000"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Drugs mostly.",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "\nUnhappy_Mongoose_778",
+        "info": "Garden. Saved me from depression. No meds ever worked.",
+        "color": "#5F76CB"
+    },
+    {
+        "user": "katzgar",
+        "info": "youtube",
+        "color": "#3B4C42"
+    },
+    {
+        "user": "Fbg_dello",
+        "info": "Smoke hella weed",
+        "color": "#FF8D3B"
+    },
+    {
+        "user": "Casperios",
+        "info": "Either escape into my own mind, or go into vr.\n\nFor me they are kinda simmilar",
+        "color": "#D6E15A"
+    },
+    {
+        "user": "TheVginyTcikler44",
+        "info": "Eat.",
+        "color": "#B10000"
+    },
+    {
+        "user": "DragonKorny",
+        "info": "Listen to an audio book in a bed with a thick warm blanket",
+        "color": "#9D84FF"
+    },
+    {
+        "user": "[deleted]",
+        "info": "it's hard work trying to find reality in this culture.\n\nEverything is a distraction from the real world.",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "misstaken66",
+        "info": "listen to music",
+        "color": "#9E22FF"
+    },
+    {
+        "user": "counterspell",
+        "info": "I pretend I won the lottery and escape to my first 24 hours of being debt free. It works to put me asleep, when I need to disassociate from a bad feeling,etc.",
+        "color": "#89F1FF"
+    },
+    {
+        "user": "TubzMcgee",
+        "info": "Think about yo momma",
+        "color": "#878687"
+    },
+    {
+        "user": "[deleted]",
+        "info": "You really need to try lucid dreaming, it takes time but is the best there are several books you can read them for instructions and you can do anything in your dreams on your own command. It's pretty cool tbh",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "Relative_Economics27",
+        "info": "Sleep. Too depressed and broke to even try anything else"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Pray",
+        "color": "#D9D9D9"
+    },
+    {
+        "user": "MinnestoanPerson",
+        "info": "Come here, and read.\n\nEdit: just got back from school and saw this.",
+        "color": "#29BF2F",
+        "undefined": "REPLIES!"
+    },
+    {
+        "user": "palmni01",
+        "info": "I find a cozy, somewhat isolated section of a trail or creek and smoke a cigar while listening to podcasts. Can't say it'a the healthiest habit, but it gets me outdoors and makes me less anxious.",
+        "color": "#4CA45A"
+    },
+    {
+        "user": "Ancient-Pause-99",
+        "info": "watch tv",
+        "color": "#FF6EF0"
+    },
+    {
+        "user": "tarani5",
+        "info": "SciFi or Fantasy movies",
+        "color": "#FF33AD"
+    },
+    {
+        "user": "TybroV",
+        "info": "Nothing. There's no escape.",
+        "color": "#745963"
+    },
+    {
+        "user": "DeionizableNormality",
+        "info": "Imagine how things might get better in my life if I tweaked a few things, practice gratitude, meditate, read fiction, talk to my parents, learn about interesting things on Reddit or the web in general, audiobook while taking an evening walk. Sometimes I like to act as though nobody around me can see me and I slip into a child like curiosity.\n\nThe other day I was walking around my apartment complex in some kaki shorts and a plaid button down/my Fanny pack sashed around my shoulder. I went into the office area to try and find this back terrace I saw while walking around the building. I found myself in the gym where they had one of those Walmart brand peloton bikes. I decided… why not give it a go! Even though everyone was looking at me in the gym like I wasn’t supposed to be there, I went for a stationary two mile bike ride while listening to my book. Happily got off after my ride, cleaned the machine, and went on my way to continue my adventure.\n\nIt seems to me that the quality of mind you’re carrying about with you will help a ton. It’s much better to slip back into that childlike curiosity than dwell on your adult life where circumstances are only moderately malleable. Slipping into that curiosity is what really makes all of these stories about people’s disconnection so salient. Whether it’s reading, day dreaming, gaming, etc. everyone is portraying a level of childlike curiosity about their lives and minds have to offer. It’s not so serious, enjoy this life and the people around you will enjoy theirs more too.",
+        "color": "#9FC82B"
+    },
+    {
+        "user": "dontjustexists",
+        "info": "Video games",
+        "color": "#3704C7"
+    },
+    {
+        "user": "Revolutionarx",
+        "info": "Make up scenarios in my head til i sleep/nap. Smoke weed",
+        "color": "#C74CBA"
+    },
+    {
+        "user": "fortknox",
+        "info": "Cook.\n\nYou get a reward at the end and improve your skills for the next go round. Since you have to eat daily, it is also a good money saver.",
+        "color": "#CED0B4"
+    },
+    {
+        "user": "Mech-T",
+        "info": "Make a fourth wall break.",
+        "color": "#DCE4FF"
+    },
+    {
+        "user": "chris34121",
+        "info": "I am a flight simulator hobbyist, and I escape by delving into the simulator.\n\nFor over 10 years now, when life gets to me, I make an escape by perfecting my flight sim world, not much unlike a model train enthusiast creates dioramas in their basement.\n\nAircraft modelling, sound engineering, environment textures such as the sky, ground, water, and trees, buildings, airport details, there's always something to tweak and work on and improve to make actually using the simulator for simulating just a bit more fun and realistic!\n\nBut my favorite part of it all is AI traffic.\n\nMyself and a team of other enthusiasts have worked for years now to \"back-date\" the AI traffic in flight sim with schedules and aircraft going from the late 90s and early 00s all the way back to the 30s in some instances. We can choose what year we want to fly, and with a week or two of work on installing schedules, we can fly in that year in our flight sim. Currently my project has been 1998, and this has been so incredibly rewarding!\n\nI live by and love my flight sim. It has always been there for me when life is too much and continues to be there for me when I have free time. Nothing else like it on this planet 🙂",
+        "color": "#F8FFAC"
+    },
+    {
+        "user": "NeonWarCry",
+        "info": "Smoke weed, take long showers, take a hike in the nearby wilderness preserve. Or play Skyrim for the 34th time.",
+        "color": "#A62060"
+    },
+    {
+        "user": "Nyarro",
+        "info": "I create fantasy worlds and build 'em up, complete with languages, culture, people, history, etc. It's fun.",
+        "color": "#B2C5D6"
+    },
+    {
+        "user": "SensitiveFox-1567",
+        "info": "Masturbate",
+        "color": "#462F2C"
+    },
+    {
+        "user": "rooftopflith",
+        "info": "Browse Reddit for stupid lengths of time!",
+        "color": "#1EB5E5"
+    },
+    {
+        "user": "plojjj",
+        "info": "Meditation",
+        "color": "#9158B3"
+    },
+    {
+        "user": "Vinny_Lam",
+        "info": "Video games, music, and anime.",
+        "color": "#BA9100"
+    },
+    {
+        "user": "shahalam7559",
+        "info": "Just turn on ANC on my headphone and listen to my favourite music!",
+        "color": "#92D274"
+    },
+    {
+        "user": "HeatmiserElliot",
+        "info": "hike. believe it or not we still live in a world with a small enough population that there are countless places you can go to experience the world without one human being in sight. thats the good stuff",
+        "color": "#562873"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Drugs",
+        "color": "#D9D9D9",
+        "undefined": "REPLIES!"
+    },
+    {
+        "user": "Highborn0298",
+        "info": "travel through a wormhole",
+        "color": "#455621"
+    },
+    {
+        "user": "OrdinaryBrilliant717",
+        "info": "Good boxing sparring sessions helps",
+        "color": "#AB866C"
+    },
+    {
+        "user": "Orphan_Company",
+        "info": "Rope.\n\nDoing tug of war against kids is pretty cool",
+        "color": "#C53C3C"
+    },
+    {
+        "user": "1ThisIsIt1",
+        "info": "Very loud music.",
+        "color": "#B318D9"
+    },
+    {
+        "user": "subpup808",
+        "info": "I know that I might get a loooot of hate for this ,but the people that understand trauma and anxiety might understand or even just people that are respectful but anyway, I get into the headspace of a dog, I don’t know why it helps but for some reason the more relaxed thought process and not needing to care about anything slows down my non stop racing brain allowing me to actually sleep, I understand people are now going to hate but it’s what I do to feel happy so if your a decent person please don’t hate on me …",
+        "color": "#0C7022"
+    },
+    {
+        "user": "Lostinthestarscape",
+        "info": "Ketamine or similar (methoxetamine, ephenidine, Mxip). Don't do drugs - but if you do, spend a lot of time researching on bluelight before you do.",
+        "color": "#9A369C"
+    },
+    {
+        "user": "[deleted]",
+        "info": "Get high",
+        "color": "#d9D9D9"
+    },
+    {
+        "user": "imheretoleave29",
+        "info": "I lay in the grass when it’s sunny",
+        "color": "#A2B0FA"
+    }
 ];
 
 const data1 = [
